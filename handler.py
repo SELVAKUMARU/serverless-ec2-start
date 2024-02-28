6f1ef0919400bda9d885edc8d163d0a2a1c5c4a1
SELVAKUMARU <selvakumaru176@gmail.com>
2024-02-28 11:53:24 +0530

    serverless to start ec2

---
+++ handler.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+
+ec2 = boto3.resource('ec2', region_name='ap-south-1')
+def lambda_handler(event, context):
+   instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']},{'Name': 'tag:start','Values':['ec2']}])
+   for instance in instances:
+       id=instance.id
+       ec2.instances.filter(InstanceIds=[id]).stop()
+       print("Instance ID is stopped:- "+instance.id)
+   return "success"
